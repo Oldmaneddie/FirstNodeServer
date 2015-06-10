c287789bf1ca3c7f13b22336c455a63fae89ba54
Edward park <parkes.edward@gmail.com>
2015-06-10 12:00:15 -0400

    code initial server and test

---
+++ app.js
@@ -0,0 +1,11 @@
+//First testing in setting up NODE server. 
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Amazing Eddie Server\n');
+  }).listen(1337, '127.0.0.1');
+
+
+console.log('Server running at http://127.0.0.1:1337/');
